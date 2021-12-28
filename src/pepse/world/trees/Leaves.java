5165a751ec7aaec223389a2554403d057a15b489
omry alcalay <omryalcalay@gmail.com>
2021-12-28 13:04:20 +0200

    Create Leaves.java

---
+++ src/pepse/world/trees/Leaves.java
@@ -0,0 +1,19 @@
+package pepse.world.trees;
+
+import danogl.util.Vector2;
+
+public class Leaves {
+    private final float treeTopX;
+    private final float treeTopY;
+    private final int seed;
+
+    public Leaves(Vector2 treeTop, int seed){
+        this.treeTopX = treeTop.x();
+        this.treeTopY = treeTop.y();
+        this.seed = seed;
+    }
+
+    public void createLeaves(){
+        // TODO
+    }
+}
