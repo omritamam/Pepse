d883dec9dd7d27a15d87b6198bea2f208cbc5122
omry alcalay <omryalcalay@gmail.com>
2021-12-29 14:39:42 +0200

    leaves are turning

--- src/pepse/world/trees/Leaves.java
+++ src/pepse/world/trees/Leaves.java
@@ -2,6 +2,8 @@ package pepse.world.trees;
 
 import danogl.GameObject;
 import danogl.collisions.GameObjectCollection;
+import danogl.components.ScheduledTask;
+import danogl.components.Transition;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
 import pepse.util.ColorSupplier;
@@ -13,6 +15,11 @@ import java.util.function.BiPredicate;
 public class Leaves {
     private static final Color LEAF_COLOR = new Color(50, 200, 30);
     private static final String TAG = "leaf";
+    private static final Float STILL_ANGLE = -20f;
+    private static final Float TURNED_ANGLE = 30f;
+    private static final Float TURNCYCLE_LENGTH = 5f;
+    private static final Vector2 STILL_DIM = new Vector2(25, 45);
+    private static final Vector2 TURNED_DIM = new Vector2(30, 30);
 
     private final float baseLocX;
     private final float baseLocY;
@@ -20,7 +27,6 @@ public class Leaves {
     private final int layer;
     private final GameObjectCollection gameObjects;
     private final int numOfSlots;
-    private int[][] leaves;
 
     public Leaves(GameObjectCollection gameObjects, Vector2 treeTop, int layer, int numOfSlots,
                   BiPredicate<Integer, Integer> density){
@@ -29,7 +35,6 @@ public class Leaves {
         this.baseLocY = treeTop.y() - ((numOfSlots - 1) * Block.SIZE);
         this.layer = layer;
         this.numOfSlots = numOfSlots;
-        this.leaves = new int[this.numOfSlots][this.numOfSlots];
         this.densityFunc = density;
     }
 
@@ -37,18 +42,31 @@ public class Leaves {
         for (int i = 0; i < this.numOfSlots; i++) {
             for (int j = 0; j < this.numOfSlots; j++) {
                 if (this.densityFunc.test(i, j)){
-                    addLeaf(i, j);
+                    createLeaf(i, j);
                 }
             }
         }
     }
 
-    private void addLeaf(int i, int j) {
+    private void createLeaf(int i, int j) {
         float x = this.baseLocX + i * Block.SIZE;
         float y = this.baseLocY + j * Block.SIZE;
-        GameObject block = new Block(new Vector2(x, y),
+        GameObject leaf = new Block(new Vector2(x, y),
                 new RectangleRenderable(ColorSupplier.approximateColor(LEAF_COLOR)));
-        block.setTag(TAG);
-        this.gameObjects.addGameObject(block, this.layer);
+        leaf.setTag(TAG);
+        this.gameObjects.addGameObject(leaf, this.layer);
+        float randFactor = (float) Math.random();
+        // TODO change random to depend on seed
+        ScheduledTask delay = new ScheduledTask(leaf, randFactor,false, ()->{
+            Transition<Float> angleTransition = new Transition<>(leaf, leaf.renderer()::setRenderableAngle,
+                    STILL_ANGLE, TURNED_ANGLE, Transition.CUBIC_INTERPOLATOR_FLOAT,
+                    TURNCYCLE_LENGTH + randFactor,
+                    Transition.TransitionType.TRANSITION_BACK_AND_FORTH, null);
+            Transition<Vector2> widthTransition = new Transition<>(leaf, leaf::setDimensions,
+                    STILL_DIM, TURNED_DIM, Transition.CUBIC_INTERPOLATOR_VECTOR,
+                    (TURNCYCLE_LENGTH + randFactor) / 2,
+                    Transition.TransitionType.TRANSITION_BACK_AND_FORTH, null);
+        });
     }
+
 }
