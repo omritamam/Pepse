76eb70804c090ec23ab82d48189b7447f7bbc20b
Omri Tamam <omri.tamam@cs.huji.ac.il>
2022-01-02 23:43:01 +0200

    endless world

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -33,9 +33,15 @@ public class PepseGameManager extends GameManager {
     private static final int NIGHT_LAYER = Layer.FOREGROUND;
     private static final int DAYNIGHT_CYCLE = 30;
     private static final Color HALO_COLOR = new Color(255, 255, 0, 20);
+    private int CurrentMinX;
+    private int CurrentMaxX;
 
     public static EnumMap<Layers, Integer> layers = new EnumMap<Layers, Integer>(Layers.class);
     private double seed;
+    private WindowController windowController;
+    private int windowWidth;
+    private Terrain terrain;
+    private Tree treeManager;
 
     public static void main(String[] args) {
 
@@ -44,36 +50,62 @@ public class PepseGameManager extends GameManager {
 
     @Override
     public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
-        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        this.windowController = windowController;
+        super.initializeGame(imageReader,
+                soundReader,
+                inputListener,
+                windowController);
         defineLayers();
         Vector2 windowDimensions = windowController.getWindowDimensions();
         windowController.setTargetFramerate(FRAME_RATE);
         this.seed = new Random().nextGaussian() * 255;
-
+        this.CurrentMinX = (int) -(0.5f * windowDimensions.x());
+        this.CurrentMaxX =  (int) (1.5f * windowDimensions.x());
+        this.windowWidth =  (int)  windowDimensions.x();
         // create sky
         Sky.create(gameObjects(), windowDimensions, SKY_LAYER);
         // create terrain
-        Terrain terrain = new Terrain(gameObjects(), GROUND_LAYER, windowDimensions, (int) seed);
-//        int cameraDimensions = (int) super.camera().getDimensions().x();
-//        terrain.createInRange(cameraDimensions, cameraDimensions + (int) windowDimensions.x());
-        // TODO check range numbers
-        terrain.createInRange(0, 1500);
+        terrain = new Terrain(gameObjects(), GROUND_LAYER, windowDimensions, (int) seed);
+        terrain.createInRange(CurrentMinX, CurrentMaxX);
         // create day-night cycle
         Night.create(gameObjects(), NIGHT_LAYER, windowDimensions, DAYNIGHT_CYCLE);
         GameObject sun = Sun.create(gameObjects(), SUN_LAYER, windowDimensions, DAYNIGHT_CYCLE);
         GameObject sunHalo = SunHalo.create(gameObjects(), HALO_LAYER, sun, HALO_COLOR);
         sunHalo.addComponent((d) -> sunHalo.setCenter(sun.getCenter()));
         // create trees
-        Tree treeManager = new Tree(gameObjects(), TREE_LAYER, terrain::groundHeightAt, (int) this.seed);
-        // TODO check range numbers
-        treeManager.createInRange(0, 1500);
-        //TODO change topLeftCornera and Layer
+        treeManager = new Tree(gameObjects(), TREE_LAYER, terrain::groundHeightAt, (int) this.seed);
+        treeManager.createInRange(CurrentMinX, CurrentMaxX);
+        //TODO change Layer from Layer.DEFAULT to enum Layer value
         float avatarInitalLocationX = windowDimensions.y()*0.5f;
         Vector2 avaterLocation = new Vector2(avatarInitalLocationX, terrain.groundHeightAt(avatarInitalLocationX) - 1.2f * Avatar.DIEMNSIONS.y());
         Avatar avatar = Avatar.create(gameObjects(), Layer.DEFAULT, avaterLocation, inputListener, imageReader);
         setCamera(new Camera(avatar, windowController.getWindowDimensions().mult(0.5f).subtract(avaterLocation), windowController.getWindowDimensions(),
                 windowController.getWindowDimensions()));
     }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        int cameraLocationX = (int) camera().getCenter().x();
+        if(cameraLocationX - windowWidth < CurrentMinX){
+            terrain.createInRange(cameraLocationX - windowWidth, CurrentMinX);
+            treeManager.createInRange(cameraLocationX - windowWidth, CurrentMinX);
+            CurrentMinX = cameraLocationX - windowWidth;
+        } if(cameraLocationX + windowWidth > CurrentMaxX){
+            terrain.createInRange(CurrentMaxX, cameraLocationX + windowWidth );
+            treeManager.createInRange(CurrentMaxX, cameraLocationX + windowWidth );
+            CurrentMaxX = cameraLocationX + windowWidth;
+        }
+        for(GameObject gameObject : gameObjects()){
+            float locationX = gameObject.getTopLeftCorner().x();
+            if(locationX<CurrentMinX || locationX > CurrentMaxX){
+                //TODO: wont work on object that arent in the deafault layer
+                gameObjects().removeGameObject(gameObject);
+
+            }
+        }
+    }
+
     private void defineLayers(){
         this.layers.put(Layers.SKY, SKY_LAYER);
         this.layers.put(Layers.SUN, SUN_LAYER);
